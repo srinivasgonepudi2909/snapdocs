1b69200d4605f2104a0bb202bb2b50ed37c384f8
srinivas <srigonepudi@gmail.com>
2025-08-09 22:54:32 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import HomeHero from "./components/HomeHero";
 import CategoryGrid from "./components/CategoryGrid";
-import WhyChooseUs from "./components/WhyChooseUs"; // ✅ Add this
+import WhyChooseUs from "./components/WhyChoose"; // ✅ Add this
 
 export default function App() {
   return (
