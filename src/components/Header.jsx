f2bdc8c10105105eafef02a8f8dbccf7fa023eb4
srinivas <srigonepudi@gmail.com>
2025-08-09 21:30:01 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,17 +5,17 @@ import logo from "../assets/icons/snapdocs-logo.png";
 export default function Header() {
   return (
     <header className="sd-header">
-      {/* Left side: Login & Sign Up */}
-      <nav className="sd-nav-left">
-        <a className="sd-btn sd-btn--blue-outline" href="/login">Login</a>
-        <a className="sd-btn sd-btn--blue" href="/signup">Sign up</a>
-      </nav>
-
-      {/* Right side: SnapDocs logo + name */}
+      {/* Left side: Logo + App name */}
       <div className="sd-brand">
         <img src={logo} alt="SnapDocs logo" className="sd-brand__logo" />
         <span className="sd-brand__name">SnapDocs</span>
       </div>
+
+      {/* Right side: Login & Sign Up */}
+      <nav className="sd-nav-right">
+        <a className="sd-btn sd-btn--blue-outline" href="/login">Login</a>
+        <a className="sd-btn sd-btn--blue" href="/signup">Sign up</a>
+      </nav>
     </header>
   );
 }
